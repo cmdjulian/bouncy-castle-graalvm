32abe9a8807a4f882eaa797010c64255ceca4886
Julian Goede <julian.goede@pm.me>
2023-07-03 21:38:15 +0200

    more settings for github package registrz

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,8 @@ subprojects {
                     name = "GitHubPackages"
                     url = uri("https://maven.pkg.github.com/cmdjulian/kirc")
                     credentials {
-                        username = project.findProperty("gpr.user") as? String? ?: System.getenv("USERNAME")
-                        password = project.findProperty("gpr.key") as? String? ?: System.getenv("TOKEN")
+                        username = project.findProperty("gpr.user") as? String? ?: System.getenv("GITHUB_ACTOR")
+                        password = project.findProperty("gpr.key") as? String? ?: System.getenv("GITHUB_TOKEN")
                     }
                 }
             }
